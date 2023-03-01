5eb57809a6c73ea58d3febf0370b99546a855f26
eman obeidat <amoonobeidat43@gmail.com>
2023-03-01 10:54:38 +0000

    final

---
+++ app.js
@@ -0,0 +1,32 @@
+var userName=prompt("what is your name?");
+console.log(userName);
+var gender=prompt("what is your gender?");
+if(gender=="female" || gender=="male")
+{
+    console.log(gender);
+
+}
+else
+{
+    alert("sorry! please enter your gender");
+    gender=prompt("what is your gender?");
+}
+var age=prompt("what is your age?");
+if(age<=0)
+{
+    alert("sorry!")
+}
+var welcoming=confirm("welcome to our page");
+if(gender=="female")
+{
+    alert("welcome Ms"+userName);
+}
+else if(gender=="male")
+{
+alert(`welcome Mr ${userName}` );
+
+}
+else
+{
+    alert("welcome :)");
+}
