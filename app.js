3cc70f1d2ddd21b5819ce48d6c1bb79408561c87
eman obeidat <amoonobeidat43@gmail.com>
2023-03-02 21:05:07 +0000

    js2

--- app.js
+++ app.js
@@ -1,40 +1,73 @@
-var userName=prompt("what is your name?");
+var userName = prompt("what is your name?");
 console.log(userName);
-var gender=prompt("what is your gender?");
-if(gender=="female" || gender=="male")
+var age = prompt("what is your age?");
+if (age <= 0) {
+    alert("sorry!")
+}
+else
 {
+    console.log(gender);
+}
+var gender = prompt("what is your gender?");
+if (gender == "female" || gender == "male") {
     console.log(gender);
 
 }
-else
-{
+else {
     alert("sorry! please enter your gender");
-    gender=prompt("what is your gender?");
+    gender = prompt("what is your gender?");
 }
-if(gender=="female")
-{
-    alert(`welcome Ms ${userName}` );
+if (gender == "female") {
+    alert(`welcome Ms ${userName}`);
 }
-else if(gender=="male")
-{
-alert(`welcome Mr ${userName}` );
+else if (gender == "male") {
+    alert(`welcome Mr ${userName}`);
 
 }
-else
-{
+else {
     alert("welcome :)");
 }
 
-var age=prompt("what is your age?");
-if(age<=0)
-{
-    alert("sorry!")
-}
-var welcoming=confirm("welcome to our page");
-var skip=confirm("do you want to skip the welcoming message")
-if(skip==true)
-{
+
+var welcoming = confirm("welcome to our page");
+var skip = confirm("do you want to skip the welcoming message");
+if (skip == true) {
     alert("Are you sure?");
 }
+var arrayOfAnswers=[];
+function causeOfAttend() {
+    var taskQuastion = confirm("Are you here to know your task?");
+    console.log(arrayOfAnswers.push(taskQuastion));
+    // console.log(taskQuastion);
+
+    // if (taskQuastion == false) {
+    //     var why = prompt("why you are here?");
+    //     console.log(why);
+    // }
+    // else if (taskQuastion == true) {
+    //     var well = alert("well done :)");
+    // }
+}
+causeOfAttend();
+
+function employeeId() {
+    var idNum = confirm("Do you have ID number?");
+    console.log(arrayOfAnswers.push(idNum));
+    console.log(idNum);
+
+}
+employeeId();
+function nationality()
+{
+    var nationalityConfirm=confirm("Are you Jordainian?");
+   console.log(arrayOfAnswers.push(nationalityConfirm));
+}
+nationality();
+console.log(arrayOfAnswers);
+// function maritalStatus()
+// {
+//     var depNum=confirm("Are you single?");
+// }
+
 
 
