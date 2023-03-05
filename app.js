56122ae99f16ccc32f7469a605e22f812bf6c6d2
eman obeidat <amoonobeidat43@gmail.com>
2023-03-05 08:11:47 +0000

    hh

--- app.js
+++ app.js
@@ -37,11 +37,12 @@ if (skip == true) {
 var arrayOfAnswers = [];
 function causeOfAttend() {
     var taskQuastion = prompt("Are you here to know your task?");
-    if (taskQuastion == "yes" || taskQuastion == "no")
-     { console.log(arrayOfAnswers.push(taskQuastion)); }
-    else {
-        console.log(arrayOfAnswers.push("invalid"));
+    if (taskQuastion == "yes" || taskQuastion == "no") { console.log(arrayOfAnswers.push(taskQuastion)); }
+    else if (taskQuastion == " "|| taskQuastion==true || taskQuastion==false) {
+        var emptyQuastion = prompt("INVALID:please answer if you are here to know your task")
+        console.log(arrayOfAnswers.push(emptyQuastion))
     }
+    else { console.log(arrayOfAnswers.push("invalid")); }
 
 
 }
@@ -49,10 +50,13 @@ causeOfAttend();
 
 function employeeId() {
     var idNum = prompt("Do you have ID number?");
-    if (idNum == "yes" || idNum == "no")
-   {console.log(arrayOfAnswers.push(idNum));} 
-    else
+    if (idNum == "yes" || idNum == "no") { console.log(arrayOfAnswers.push(idNum)); }
+    else if (idNum == " " || idNum==true ||idNum==false) 
     {
+        var emptyid = prompt("INVALID:please enter if you have id number or not")
+        console.log(arrayOfAnswers.push(emptyid))
+    }
+    else {
         console.log(arrayOfAnswers.push("invalid"));
     }
 }
@@ -60,12 +64,15 @@ employeeId();
 
 function nationality() {
     var nationalityConfirm = prompt("Are you Jordainian?");
-    if (nationalityConfirm == "yes" || nationalityConfirm == "no")
-   {    console.log(arrayOfAnswers.push(nationalityConfirm));}
-    
-    else
-    {
+    if (nationalityConfirm == "yes" || nationalityConfirm == "no") { console.log(arrayOfAnswers.push(nationalityConfirm)); }
+
+    else if (nationalityConfirm == "") {
+        var invalidnation = prompt("INVALID:please answer if you are jordanian or not");
+        console.log(arrayOfAnswers.push(invalidnation));
+    }
+    else {
         console.log(arrayOfAnswers.push("invalid"));
+
     }
 }
 nationality();
