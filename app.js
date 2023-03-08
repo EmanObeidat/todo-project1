5443d4402e50fa2e72adf4e3c381c3d9a0ddc327
eman obeidat <amoonobeidat43@gmail.com>
2023-03-08 11:12:47 +0000

    updatelab6

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+`use strict`;
 var userName = prompt("what is your name?");
 console.log(userName);
 var age = prompt("what is your age?");
@@ -16,19 +17,6 @@ else {
     alert("sorry! please enter your gender");
     gender = prompt("what is your gender?");
 }
-// if (gender == "female") {
-//     alert(`welcome Ms ${userName}`);
-// }
-// else if (gender == "male") {
-//     alert(`welcome Mr ${userName}`);
-
-// }
-// else {
-//     alert("welcome :)");
-// }
-
-
-// var welcoming = confirm("welcome to our page");
 var skip = confirm("do you want to skip the welcoming message");
 if (skip !== true) {
     if (gender == "female") {
@@ -36,24 +24,20 @@ if (skip !== true) {
     }
     else if (gender == "male") {
         alert(`welcome Mr ${userName}`);
-    
+
     }
     else {
         alert("welcome :)");
-    }}
+    }
+}
 
-    `use strict`;
 
 var arrayOfAnswers = [];
 function causeOfAttend() {
     var taskQuastion = prompt("Are you here to know your task?");
-    if (taskQuastion == "yes" || taskQuastion == "no")
-     { console.log(arrayOfAnswers.push(taskQuastion)); }
-    // else if (taskQuastion == " "|| taskQuastion==true || taskQuastion==false) {
-    //     var emptyQuastion = prompt("INVALID:please answer if you are here to know your task")
-    //     console.log(arrayOfAnswers.push(emptyQuastion))
-    // }
-    else { console.log(arrayOfAnswers.push("invalid")); }
+    if (taskQuastion == "yes" || taskQuastion == "no") { console.log(arrayOfAnswers.push(taskQuastion)); }
+
+    else { arrayOfAnswers.push("invalid"); }
 
 
 }
@@ -61,35 +45,28 @@ causeOfAttend();
 
 function employeeId() {
     var idNum = prompt("Do you have ID number?");
-    if (idNum == "yes" || idNum == "no")
-     { console.log(arrayOfAnswers.push(idNum)); }
-    // else if (idNum == " " || idNum==true ||idNum==false) 
-    // {
-    //     var emptyid = prompt("INVALID:please enter if you have id number or not")
-    //     console.log(arrayOfAnswers.push(emptyid))
-    // }
+    if (idNum == "yes" || idNum == "no") { console.log(arrayOfAnswers.push(idNum)); }
+
     else {
-        console.log(arrayOfAnswers.push("invalid"));
+        arrayOfAnswers.push("invalid");
     }
 }
 employeeId();
 
 function nationality() {
     var nationalityConfirm = prompt("Are you Jordainian?");
-    if (nationalityConfirm == "yes" || nationalityConfirm == "no")
-     { console.log(arrayOfAnswers.push(nationalityConfirm)); }
+    if (nationalityConfirm == "yes" || nationalityConfirm == "no") { console.log(arrayOfAnswers.push(nationalityConfirm)); }
 
-    // else if (nationalityConfirm == "") {
-    //     var invalidnation = prompt("INVALID:please answer if you are jordanian or not");
-    //     console.log(arrayOfAnswers.push(invalidnation));
-    // }
+
     else {
-        console.log(arrayOfAnswers.push("invalid"));
+        arrayOfAnswers.push("invalid");
 
     }
 }
 nationality();
-console.log(arrayOfAnswers);
-
+for(let i=0; i<arrayOfAnswers.length;i++)
+{
+    console.log(arrayOfAnswers[i]);
+}
 
 
